57354d6c8605f7d6128c307fc08647544bc5524b
mikolaj <mikolaj.cieszczyk@gmail.com>
2023-05-17 08:36:54 +0200

    frontend folder structure cleaned, added favicon, updated manifest, removed unnecessary files, updated index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Counter from "./features/counter/Counter";
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
