0a1c8699a63a163f7cc2f06c5dc8727fd98c47a9
Mateusz Krzeminski <mateusz.krzeminski@stxnext.com>
2023-05-18 11:40:13 +0200

    fix: Change import

--- backend/app/db/config.py
+++ backend/app/db/config.py
@@ -1,7 +1,6 @@
 from app.utils.create_db_url import create_db_url
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine(create_db_url(), echo=True)
 Base = declarative_base()
