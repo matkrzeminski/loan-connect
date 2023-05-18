e6900ec56d6d89b774e833b828ad0c112a995730
mikolaj <mikolaj.cieszczyk@gmail.com>
2023-05-18 09:33:10 +0200

    feat: add basic register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import BaseLayout from "./components/organisms/BaseLayout";
 import List from "./components/molecules/List";
+import RegisterPage from "./components/pages/RegisterPage";
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <BaseLayout>
+      <RegisterPage />
+      {/* <BaseLayout>
         <List />
-      </BaseLayout>
+      </BaseLayout> */}
     </div>
   );
 }
