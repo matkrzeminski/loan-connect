f7c95424a797002f26298af2507e705fe9c49fba
mikolaj <mikolaj.cieszczyk@gmail.com>
2023-05-19 10:54:26 +0200

    feat: add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,16 @@ import BaseLayout from "./components/organisms/BaseLayout";
 import List from "./components/molecules/List";
 import RegisterPage from "./components/pages/RegisterPage";
 import LandingPage from "./components/pages/LandingPage";
+import LoginPage from "./components/pages/LoginPage";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <BaseLayout>
-        <LandingPage />
+        {/* <LandingPage /> */}
         {/* <List /> */}
         {/* <RegisterPage /> */}
+        <LoginPage />
       </BaseLayout>
     </div>
   );
